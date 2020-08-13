c2298b3e6ad88d11413a0b1dd0c3eb6343f5dbe6
ShirleneRose <51985847+ShirleneRose@users.noreply.github.com>
2020-08-13 15:34:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+
+const request = require("request");
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/signup.html");
+});
+app.post("/", function(req, res){
+  const firstName = req.body.fname;
+  const lastName = req.body.lname;
+  const email= req.body.email;
+  const data={
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields:{
+          FNAME:firstName,
+          LNAME:lastName
+        }
+      }
+    ]
+  };
+  const jsonData = JSON.stringify(data);
+  const url = "https://us17.api.mailchimp.com/3.0/lists/6071a9316a"
+  const options = {
+    method:"POST",
+    auth:"shirlene:7522b369ca0e6f3f08fb047dd0a11eb8-us17"
+  }
+const request = https.request(url, options, function(response){
+ if(response.statusCode === 200){
+   res.sendFile(__dirname + "/success.html");
+ }
+ else{
+   res.sendFile(__dirname + "/failure.html");
+ }
+
+  response.on("data", function(data){
+    console.log(JSON.parse(data));
+  })
+});
+request.write(jsonData);
+request.end();
+});
+app.post("/failure", function(req,res){
+  res.redirect("/")
+})
+app.listen(process.env.PORT, function(){
+  console.log("Server is running on port 3000");
+});
+// API Key
+// 7522b369ca0e6f3f08fb047dd0a11eb8-us17
+// Audience unique ID:6071a9316a
